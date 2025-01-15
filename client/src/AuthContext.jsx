e6235ee7fbe10ea3aeab9122d19d179fe2eebc4b
RAJJIT <2.0lairaj@gmail.com>
2025-01-15 06:49:23 +0530

    auth file added

--- client/src/AuthContext.jsx
+++ client/src/AuthContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react';
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(true);
+    const [user, setUser] = useState(null);
 
     const login = (username, password) => {
         setUser({ username });
